a67d8d667e2653ce0e5694f430eaddbd5d0583ee
ayoublaksir <75275134+ayoublaksir@users.noreply.github.com>
2024-10-20 19:40:54 +0200

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,16 @@ hamburger.addEventListener('click', () => {
 function scrollToSection(sectionId) {
     document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
 }
+
+function redirectToContest1() {
+    // Replace this URL with the contest URL for Chance 1
+    const contestUrl1 = "https://www.example.com/chance1";
+    window.location.href = contestUrl1;
+}
+
+function redirectToContest2() {
+    // Replace this URL with the contest URL for Chance 2
+    const contestUrl2 = "https://www.example.com/chance2";
+    window.location.href = contestUrl2;
+}
+
